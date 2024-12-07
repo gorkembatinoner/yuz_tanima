3ffcc3fe62143bb195b609c32536335e2febc50f
Nilay <nilaydonmez390@gmail.com>
2024-12-07 16:40:51 +0300

    Kullanıcıdan isim alma özelliği eklendi

--- add_face.py
+++ add_face.py
@@ -5,14 +5,17 @@ from database_operations import add_face_to_database  # Doğru modülü içeri a
 
 # Fotoğrafı yükleyip encoding verisini çıkarma fonksiyonu
 def process_and_add_face(image_path):
-   
+    # Fotoğrafı yükle
     image = face_recognition.load_image_file(image_path)
-  
+
+    # Yüz encoding verisini çıkar
     encoding = face_recognition.face_encodings(image)
 
     if encoding:
+        # Kullanıcıdan bir isim iste
+        name = input("Bu yüz için bir isim girin (örneğin: Nilay): ")
+
         # Encoding verisini veritabanına ekle
-        name = os.path.basename(image_path).split('.')[0]  # Fotoğrafın ismini al (örneğin: 'nilay')
         add_face_to_database(name, np.array(encoding[0]))
         print(f"Veri başarıyla eklendi: {name}")
     else:
@@ -22,5 +25,3 @@ def process_and_add_face(image_path):
 if __name__ == "__main__":
     image_path = input("Fotoğraf dosyasının yolunu girin (örneğin: faces/nilay.png): ")
     process_and_add_face(image_path)
-
-
